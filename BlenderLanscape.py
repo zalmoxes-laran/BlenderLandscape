112ad628db21d2ae1e30fbbb67fccecc87e82c6b
zalmoxes-laran <emanuel.demetrescu@gmail.com>
2016-07-24 11:14:07 +0200

    Added support for LOD2 generation

--- BlenderLanscape.py
+++ BlenderLanscape.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "BlenderLandscape",
     "author": "E. Demetrescu",
-    "version": (1,2),
+    "version": (1,2.1),
     "blender": (2, 7, 7),
     "api": 48000,
     "location": "Tool Shelf panel",
@@ -130,7 +130,7 @@ class ToolsPanel2(bpy.types.Panel):
     bl_region_type = "TOOLS"
     bl_context = "objectmode"
     bl_category = "BL"
-    bl_label = "Automator"
+    bl_label = "LOD generator"
      
     def draw(self, context):
         layout = self.layout        
@@ -140,11 +140,11 @@ class ToolsPanel2(bpy.types.Panel):
         row = layout.row()
         row.label(text="Active object is: " + obj.name)
         row = layout.row()
-        self.layout.operator("automator.b2osg", icon="COLOR", text='Automator')
+        self.layout.operator("lod2.b2osg", icon="COLOR", text='LOD 2')
         row = layout.row()
         row.label(text="Resulting files: ")
         row = layout.row()
-        row.label(text= obj.name + "-inst.txt and " + obj.name + ".obj" )
+        row.label(text= "LOD2_"+ obj.name + ".obj" )
         row = layout.row()
 
 class OBJECT_OT_ExportButtonName(bpy.types.Operator):
@@ -463,7 +463,7 @@ class CreateCameraImagePlane(bpy.types.Operator):
 
 
 class OBJECT_OT_Automator(bpy.types.Operator):
-    bl_idname = "automator.b2osg"
+    bl_idname = "lod2.b2osg"
     bl_label = "Automator"
     bl_options = {"REGISTER", "UNDO"}
     
@@ -473,23 +473,72 @@ class OBJECT_OT_Automator(bpy.types.Operator):
         
         if not basedir:
             raise Exception("Il file Blender non è stato salvato, prima salvalo per la miseria !")
+        for obj in bpy.context.selected_objects:
+            baseobj = obj.name
+        bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "constraint_axis":(False, False, False), "constraint_orientation":'GLOBAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
+        for obj in bpy.context.selected_objects:
+            obj.name = "LOD2_" + baseobj
+            newobj = obj
+        for obj in bpy.context.selected_objects:
+            lod2name = obj.name
+        #--------------------------------------------------------------------
 
-        selection = bpy.context.selected_objects
+        for i in range(0,len(bpy.data.objects[lod2name].material_slots)):
+            bpy.ops.object.material_slot_remove()
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.uv.select_all(action='SELECT')
+        bpy.ops.uv.pack_islands(margin=0.001)
+
+        # procedura di semplificazione mesh
+        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.ops.mesh.select_non_manifold()
+        bpy.ops.object.vertex_group_add()
+        bpy.ops.object.vertex_group_assign()
+        bpy.ops.object.editmode_toggle()
+        bpy.data.objects[lod2name].modifiers.new("Decimate", type='DECIMATE')
+        bpy.context.object.modifiers["Decimate"].ratio = 0.1
+        bpy.context.object.modifiers["Decimate"].vertex_group = "Group"
+        bpy.context.object.modifiers["Decimate"].invert_vertex_group = True
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate")
+        # ora la mesh è semplificata
+        #------------------------------------------------------------------
         bpy.ops.object.select_all(action='DESELECT')
+        oggetto = bpy.data.objects[lod2name]
+        oggetto.select = True
+
+        tempimage = bpy.data.images.new(name=lod2name, width=512, height=512, alpha=False)
+        tempimage.filepath_raw = "//"+lod2name+".jpg"
+        tempimage.file_format = 'JPEG'
+
+        for uv_face in oggetto.data.uv_textures.active.data:
+            uv_face.image = tempimage
+
+        #--------------------------------------------------------------
+        bpy.context.scene.render.engine = 'BLENDER_RENDER'
+        bpy.context.scene.render.use_bake_selected_to_active = True
+        bpy.context.scene.render.bake_type = 'TEXTURE'
+
+        object = bpy.data.objects[baseobj]
+        object.select = True
+
+        bpy.context.scene.objects.active = bpy.data.objects[lod2name]
+        #--------------------------------------------------------------
+
+        bpy.ops.object.bake_image()
+        tempimage.save()
+
+        bpy.ops.object.select_all(action='DESELECT')
+        oggetto = bpy.data.objects[lod2name]
+        oggetto.select = True
+
+#        basedir = os.path.dirname(bpy.data.filepath)
         activename = bpy.path.clean_name(bpy.context.scene.objects.active.name)
         fn = os.path.join(basedir, activename)
-        file = open(fn + "-inst.txt", 'w')
-        
-        # write selected objects coordinate
-        for obj in selection:    
-            obj.select = True  
-            file.write("%s %s %s %s %s %s %s %s %s\n" % (obj.location[0], obj.location[1], obj.location[2], obj.rotation_euler[0], obj.rotation_euler[1], obj.rotation_euler[2], obj.scale[0], obj.scale[1], obj.scale[2]))
-        file.close()
-        
-        active = bpy.context.scene.objects.active
-        bpy.ops.object.select_all(action='DESELECT')
-        active.select = True
         bpy.ops.export_scene.obj(filepath=fn + ".obj", use_selection=True, axis_forward='Y', axis_up='Z', path_mode='RELATIVE')
+
+        bpy.ops.object.move_to_layer(layers=(False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+        bpy.context.scene.layers[1] = True
+        bpy.context.scene.layers[0] = False
         return {'FINISHED'}
 
 
