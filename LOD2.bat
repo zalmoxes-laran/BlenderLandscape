463d3b53600093a4d338021301769d5f3fa97783
emanuel <emanuel.demetrescu@gmail.com>
2018-01-02 19:55:50 +0100

    clean up of code snippets

--- LOD2.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-ECHO Questo file creerà un file obj di livello LOD2 a partire da un file obj di partenza (via Blender che deve essere installato nel sistema)
-PAUSE
-REM copy NUL %1.blend
-"C:\Program Files\Blender Foundation\Blender\blender.exe" %1.blend -P LOD2_standalone.py
